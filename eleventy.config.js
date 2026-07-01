5feccd81f2c6f14b0fa9be8043d5a805fc60b710
Savy <savy.novachez011@proton.me>
2026-07-01 10:20:41 +0530

    setup yaml data files

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,11 @@
+import YAML from "yaml";
+
 export default function (cfg) {
   cfg.setServerOptions({
     port: 1212,
   });
+
+  cfg.addDataExtension("yaml,yml", contents => YAML.parse(contents));
 
   return {
     markdownTemplateEngine: "njk",
